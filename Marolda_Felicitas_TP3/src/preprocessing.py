df83e29cf0810dc2ddd041afb19ec393df3abadd
felicitasmarolda <fmarolda@udesa.edu.ar>
2025-05-04 11:36:30 -0300

    PREPROCESAMIENTO

---
+++ Marolda_Felicitas_TP3/src/preprocessing.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def split_data(X, y, test_size = 0.2, val_size = 0.2, random_state = 42):
+    np.random.seed(random_state)
+    
+    indices = np.arange(X.shape[0])
+    np.random.shuffle(indices)
+    
+    test_size = int(len(X) * test_size)
+    val_size = int(len(X) * val_size)
+    
+    X_test = X[indices[:test_size]]
+    y_test = y[indices[:test_size]]
+    
+    X_val = X[indices[test_size:test_size + val_size]]
+    y_val = y[indices[test_size:test_size + val_size]]
+    
+    X_train = X[indices[test_size + val_size:]]
+    y_train = y[indices[test_size + val_size:]]
+    
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+def normalize(X, number = 255):
+    # dividimos todo por 255
+    X = X / number
+    return X
